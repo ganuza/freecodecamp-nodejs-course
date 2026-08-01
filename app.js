f8ea0f73970f97f7913fa928b1832720ac8df876
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-01 08:47:36 -0600

    Create new files for names and utils and move code

---
+++ app.js
@@ -0,0 +1,10 @@
+// Modules - Encapsulated Code (only share the minimum)
+
+// we'll be running our code by executing one file
+// BUT, we'll be splitting our code into modules
+// it would be insane to jam all of our code into one file
+// modules allow us to separate code into separate files that we can access throughout our application
+// this allows us to have smaller files and add structure to our app
+
+sayHello('Susan');
+sayHello(john);
