820219eb3edf3c95ab99c14ddd56c38192b98026
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-03 17:14:55 -0600

    Add export for sayHello function in utils.js

--- 5-utils.js
+++ 5-utils.js
@@ -1,3 +1,7 @@
 const sayHello = (name) => {
   console.log(`Hello ${name}`);
 };
+
+module.exports = sayHello; // you don't need to put it into an object since it is only 1 thing
+
+console.log('utils module: ', module); // you see the function in the module.exports object
