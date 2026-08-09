5d5615c4e6e27cadcf9be68129dd087c5452fb58
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-09 11:31:50 -0600

    Add more notes to 6-alternative-exports

--- 6-alternative-exports.js
+++ 6-alternative-exports.js
@@ -11,3 +11,7 @@ module.exports.items = ['item1', 'item2'];
 // we can also do it this way:
 module.exports.singlePerson = person;
 // same here, we set a singlePerson property on the module.exports object and assign it to our person object
+
+// when you console log the module exports property, you see that both data types were added as properties
+// you'll see the module.exports contains 2 propeties now, 1 for each export
+console.log('module exports: ', module.exports);
