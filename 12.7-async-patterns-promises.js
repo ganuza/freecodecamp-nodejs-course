36caf14f9312b66c57a60cbe716b532288a220df
Adrian Zabolitzki <adrianzab@gmail.com>
2026-08-31 11:16:10 -0600

    Archive async patterns code and notes

---
+++ 12.7-async-patterns-promises.js
@@ -0,0 +1,67 @@
+// re-create the readFile() writeFile() setup
+
+import fs, { readFile, writeFile } from 'node:fs/promises';
+// import util, { promisify } from 'util';
+
+// console.log('util: ', util);
+// const readFilePromise = promisify(readFile);
+// const writeFilePromise = promisify(writeFile);
+// create a new function to get text using promise
+// we want to take in a path and return a promise
+// then move the readFile function into the new function
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf8', (err, result) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
+//       const firstFileText = result;
+//       resolve(result);
+//     });
+//   });
+// };
+
+const showText = async () => {
+  try {
+    const firstText = await readFile('./content/first.txt', 'utf8');
+    console.log('Success: ', firstText);
+    const secondText = await readFile('./content/second.txt', 'utf8');
+    console.log('Success: ', secondText);
+    const newText = await writeFile(
+      './content/result-promisify-write-file.txt',
+      `${firstText}, ${secondText}`,
+    );
+  } catch (error) {
+    console.log('Failed: ', error);
+  }
+};
+
+showText();
+
+// now we can invoke getText and it is going to return a promise
+// we can now chain a .then and .catch for the error
+
+// first text
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log('*Error: ', err));
+
+//   readFile('./content/second.txt', 'utf8', (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     const secondFileText = result;
+//     writeFile(
+//       './content/result-2-write-file-async.txt',
+//       `Here is the result: ${firstFileText}, ${secondFileText}`,
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         console.log('writeFile result: ', result);
+//       },
+//     );
+//   });
+// });
