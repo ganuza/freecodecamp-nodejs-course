1aa630b446cf7b7d8af5fc618ee3c4bea256f698
Adrian Zabolitzki <adrianzab@gmail.com>
2026-09-06 13:04:55 -0600

    Create bigger text file

--- 15-create-big-file.js
+++ 15-create-big-file.js
@@ -2,6 +2,6 @@ import { writeFileSync } from 'fs';
 
 console.log('writeFileSync: ', writeFileSync);
 
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < 100000; i++) {
   writeFileSync('./content/big-file.txt', 'Hello World!', { flag: 'a' });
 }
